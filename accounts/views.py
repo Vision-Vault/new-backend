7601aa09303b790035d95484e11e21717c078464
Husam Obeidat <husamobe1991@gmail.com>
2023-08-31 21:11:18 +0000

    fixed the permission for Create User

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
 from .models import CustomUser
 from .serializers import CustomUserSerializer,detailUserSerializer
+from rest_framework.permissions import AllowAny
 
 class CustomUserList(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classe = AllowAny
 
 class CustomUserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
