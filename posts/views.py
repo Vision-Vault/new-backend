db517d457a7b39edc798c4f2c7f107c5343ac5c1
AbdToughoj <toughoj1@gmail.com>
2023-09-04 20:07:45 +0300

    create-update-get-rating

--- posts/views.py
+++ posts/views.py
@@ -2,10 +2,14 @@ from rest_framework import generics,status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerAndUpdateFields
-from .models import Category, Post,Donation
-from .serializers import CategorySerializer, PostSerializer,DonationSerializer
+from .models import Category, Post,Donation, Rating
+from .serializers import CategorySerializer, PostSerializer,DonationSerializer,RatingSerializer
 from rest_framework.response import Response
 from accounts.models import CustomUser
+from django.db.models import Sum
+from django.db.models import Count
+
+
 
 
 
@@ -45,7 +49,7 @@ class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.filter(status=Post.ACTIVE)
-    serializer_class = PostSerializer
+    serializer_class = RatingSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -53,7 +57,72 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerAndUpdateFields]
 
+class PostUsersRatings(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerAndUpdateFields]
+    serializer_class = RatingSerializer
 
+    def get_queryset(self):
+        pk = self.kwargs['pk']  
+        post = Post.objects.get(id=pk)  
+        queryset = Rating.objects.filter(post=post) 
+        return queryset
+
+    def get(self, request, pk, *args, **kwargs):
+        queryset = self.get_queryset()  
+        serializer = self.get_serializer(queryset, many=True) 
+        return Response({"data":serializer.data})
+
+class RatePost(APIView):
+    def post(self, request , post_id):
+        try:
+            args = request.data.copy()
+            rating = args.get("rate" , None) 
+            user_id = args.get("user" , None) 
+
+            # Get post record 
+            post = Post.objects.get(pk=post_id)
+
+            # Get user record
+            try: 
+                user = CustomUser.objects.get(pk=user_id)
+            except Exception as error :
+                return Response({'error': "You have to login to add you rating on this post"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+            # Check if rating already provided 
+            try:
+                old_rating = Rating.objects.get(user=user, post=post)
+            except Exception as error :
+                old_rating = None
+
+            if old_rating == None :
+                # Create
+                new_rating = Rating.objects.create(user=user, post=post, value=rating)
+            else:
+                # Update
+                new_rating = Rating.objects.filter(user=user, post=post).update(value=rating)
+            
+            # Update post rating 
+            sum_record = Rating.objects.aggregate(Sum('value'))
+            count_record = Rating.objects.filter(post = post).values("post_id").annotate(Count("post_id"))
+            sum_value = sum_record.get("value__sum",0)
+            count_value = count_record[0].get("post_id__count",0)
+
+            #Calculate total rating on post 
+            total_rating = sum_value/count_value
+
+            # Update post rating
+            Post.objects.filter(id=post_id).update(rating=total_rating)
+
+            return Response({'data': "Rating placed successfully!"}, status=status.HTTP_200_OK)
+
+        except Exception as error :
+            return Response({'error': error}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+    # get rating 
+    # insert rsting to post 
 
 class DonationCreateView(APIView):
     def post(self, request, post_id):
