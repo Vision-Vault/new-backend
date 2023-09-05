50a94ab60654dad3365124afe299d90e0ba886b3
Husam Obeidat <husamobe1991@gmail.com>
2023-09-05 20:14:22 +0000

    now we can get more data from the comment id

--- comments/serializers.py
+++ comments/serializers.py
@@ -5,8 +5,26 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        depth = 0
+
+    def __init__(self, instance=None, data=None, **kwargs):
+        if data :
+            setattr(self.Meta , 'depth', 0)
+        else:
+            setattr(self.Meta , 'depth', 2)
+
+        super(CommentSerializer, self).__init__(instance ,data, **kwargs)
 
 class ChildCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChildComment
         fields = '__all__'
+        depth = 0
+
+    def __init__(self, instance=None, data=None, **kwargs):
+        if data :
+            setattr(self.Meta , 'depth', 0)
+        else:
+            setattr(self.Meta , 'depth', 2)
+
+        super(ChildCommentSerializer, self).__init__(instance ,data, **kwargs)
--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post,Category,Donation
+from .models import Post,Category,Donation,Rating
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Donation)
+admin.site.register(Rating)
