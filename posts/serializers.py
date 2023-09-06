ef458f34c784d90333f4447eb1804177c0efc8d9
Husam Obeidat <husamobe1991@gmail.com>
2023-09-06 08:48:21 +0000

    removed depth

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,15 +10,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
-        depth = 0
 
-    def __init__(self, instance=None, data=None, **kwargs):
-        if data :
-            setattr(self.Meta , 'depth', 0)
-        else:
-            setattr(self.Meta , 'depth', 2)
-
-        super(PostSerializer, self).__init__(instance ,data, **kwargs)
 
 
 class DonationSerializer(serializers.ModelSerializer):
